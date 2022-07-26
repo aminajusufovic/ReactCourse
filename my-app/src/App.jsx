3a75f339a1f7ab31305b03a8a92632c68fc902ea
Amina <aminajusufovic4@gmail.com>
2022-07-26 14:23:32 +0200

    commit

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -8,15 +8,6 @@ const App = () = {
     
         const res = await fetch(
             "http://random-data-api.com/api/dessert/random_dessert?size=100");
-    
-    const data = await res.json();
-    setDesserts(data);
-    console.log(data);
-    };
-    useEffect(() => {
-    getDesserts();
-}, [])
-;    
-return <div{ Desserts.map((dessert) => {return <DessertCard des={dessert} })}/>;
+   
 };
 export default App;
